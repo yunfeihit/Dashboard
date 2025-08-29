60545368e31fc0933cb168f9eb123504dbec3d86
yunfeihit <157768744+yunfeihit@users.noreply.github.com>
2025-08-29 03:31:38 +0000

    update the link

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
-const iframeContainerSingUpForm = document.querySelector('#project-sign-up-form .iframe-container')；
+const iframeContainerSingUpForm = document.querySelector('#project-sign-up-form .iframe-container');
 const linkSignUpForm = iframeContainerSingUpForm.querySelector('a');
 iframeContainerSingUpForm.addEventListener("click", () => {
-    window.open(link.href, link.target);
+    window.open(linkSignUpForm.href, linkSignUpForm.target);
 })
